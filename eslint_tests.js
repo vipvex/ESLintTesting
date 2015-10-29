04f63ea4d35939239177c433f8817158afc68d04
vipvex <Aleksander.battle@gmail.com>
2015-10-29 12:02:19 -0700

    Update eslint_tests.js

--- eslint_tests.js
+++ eslint_tests.js
@@ -30,23 +30,41 @@ function checkIfVarExists(context) {
     "validate-variables" : [2, 
                             "variables" : {
                                 "name" : "Jon Smith",
-                                "age"  : 12
-                                "debt" : 9001
+                                "age"  : 12,
+                                "debt" : 9001,
                                 "description" : null
                             }, 
                             exclusive: true] // Must only be these variables 
 */
+
 function validateVariables(context) {
     return {
         VariableDeclarator: function (node) {
             
-            // check if exliusive
-            
-                // if exluisive check if var name matchest one of the variables
-                // check if var has value
-                // check if vars value matches with what is expected
+            // Retreive the variable paramaters
+            var variables = context.options[0];
+            var exclusive = context.options[1];
             
             var varName = node.id.name;
+            var varVal  = node.init.value;
+        
+            if (varName in variables){
+
+                context.report(node, "Found variable decloration: " + varName, data: { correct: true });
+            
+                if (varVal == variables[varName])
+                {
+                    context.report(node, "Variable " + varName + " correctly set to " + varName, data: { correct: true });
+                }
+ 
+            }
+  
+            
+            // if exluisive check if var name matchest one of the variables
+            // check if var has value
+            // check if vars value matches with what is expected
+            
+            
             if (!(varName == context.options[0])) {
                 context.report(node, "Variable not named myVar");
             }
