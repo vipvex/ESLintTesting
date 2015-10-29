da495e5c6bac5b6e0c749897f7692c13d5878804
vipvex <Aleksander.battle@gmail.com>
2015-10-29 12:39:59 -0700

    Update eslint_tests.js

--- eslint_tests.js
+++ eslint_tests.js
@@ -78,3 +78,4 @@ function validateVariables(context) {
 
 eslint.defineRule("if-curly-formatting", ifCurlyFormatting);
 eslint.defineRule("check-if-var-exists", checkIfVarExists);
+eslint.defineRule("validate-variables",  validateVariables)
